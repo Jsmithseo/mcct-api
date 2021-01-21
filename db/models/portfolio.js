e0b40e7f40d93b40f5df07296387bc127e5fdbaf
Admin <admin@Addapps-MacBook-Pro.local>
2021-01-21 00:28:13 -0800

    WIP

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
 const portfolioSchema = new Schema({
   title: { type: String, required: true, maxlength: 128 },
   company: { type: String, required: true, maxlength: 64},
@@ -10,7 +12,7 @@ const portfolioSchema = new Schema({
   description: { type: String, required: true },
   startDate: { type: Date, required: true},
   endDate: { type: Date },
-  userId: { type: String, required: true },
+  userId: { type: mongoose.SchemaTypes.ObjectId, ref: "user"},
   createdAt: { type: Date, default: Date.now }
 })
 
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-
 const { checkJwt } = require('../controllers/auth');
 const {
   getPortfolios,
   getPortfolioById,
-  createPortfolio } = require('../controllers/portfolios');
+  createPortfolio,
+  updatePortfolio } = require('../controllers/portfolios');
 
 router.get('', getPortfolios);
 router.get('/:id', getPortfolioById);
 
+// TODO: create middleware to check for admin rights!!!!
 router.post('', checkJwt, createPortfolio);
+router.patch('/:id', checkJwt, updatePortfolio);
 
 module.exports = router;
