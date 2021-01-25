e1e92dfb9a34b8703bc0903931346570ea10eb4f
Admin <admin@Addapps-MacBook-Pro.local>
2021-01-24 19:59:32 -0800

    WIP

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DB_URI: "mongodb+srv://softwaresmitty:J77NqeUhbKnuHLx@cluster0.dxotk.mongodb.net/MCCT?retryWrites=true&w=majority",
+    AUTH0_NAMESPACE: "AUTH0_NAMESPACE", // Get from https://auth0.com/
+    AUTH0_TOKEN_URL: "AUTH0_TOKEN_URL",
+    AUTH0_CLIENT_ID: "AUTH0_CLIENT_ID",
+    AUTH0_CLIENT_SECRET: "AUTH0_CLIENT_SECRET",
+    AUTH0_AUDIENCE: "AUTH0_AUDIENCE",
+    AUTH0_DOMAIN: "AUTH0_DOMAIN",
+  }
